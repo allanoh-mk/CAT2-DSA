ab601be5435913ba7c0e372d495599a4f22dc3b4
192288 <allan.wachuka@strathmore.edu>
2025-03-23 22:23:41 +0300

    Initial commit

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,4 @@
+// Node class to represent elements in the linked list
 class Node {
     int data;
     Node next;
@@ -8,27 +9,34 @@ class Node {
     }
 }
 
+// Linked List class with basic operations
 public class LinkedList {
-    Node head; 
+    Node head; // Points to the first node
+
+    // Insert at the beginning
     public void insertAtBeginning(int data) {
         Node newNode = new Node(data);
-        newNode.next = head; 
-        head = newNode; 
+        newNode.next = head; // New node points to current head
+        head = newNode; // Update head to the new node
     }
+
+    // Insert at the end
     public void insertAtEnd(int data) {
         Node newNode = new Node(data);
         if (head == null) {
-            head = newNode; 
+            head = newNode; // List is empty; new node becomes head
             return;
         }
         Node current = head;
         while (current.next != null) {
-            current = current.next; 
+            current = current.next; // Traverse to last node
         }
-        current.next = newNode; 
+        current.next = newNode; // Add new node at end
     }
+
+    // Delete from the beginning
     public void deleteFromBeginning() {
-        if (head == null) return; 
-        head = head.next;
+        if (head == null) return; // List is empty
+        head = head.next; // Move head to the next node
     }
 }
